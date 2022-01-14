646b5c6baa6a9ba82b4a8fedf0e35a2cdbe5d9eb
RolandASP <roland@adsitepro.com>
2022-01-14 13:40:00 -0800

    initial commit

---
+++ public/frontend/footer.php
@@ -0,0 +1,303 @@
+	<!-- Footer -->
+	<footer class="bg3 p-t-20 p-b-0 footer-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3 col-lg-3 p-b-5">
+					<center><h4 class="stext-301 cl0 p-b-10" style="color:orange;">
+						Resources
+					</h4>
+					</center>
+
+					<center>
+					<ul>
+						<li class="p-b-10">
+							<a href="about.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								About G2DS
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="domains.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Domains for Sale
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="apps.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Apps for Sale
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="hosting.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Hosting
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="webdev.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Web Development
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="careers.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Careers
+							</a>
+						</li>
+
+
+					</ul>
+					</center>
+				</div>
+
+				<div class="col-sm-6 col-lg-3 p-b-5">
+					<center><h4 class="stext-301 cl0 p-b-10" style="color:orange;">
+						Help
+					</h4></center>
+
+					<center><ul>
+						<li class="p-b-10">
+							<a href="login.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Login
+							</a>
+						</li>
+						<li class="p-b-10">
+							<a href="register.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Registration
+							</a>
+						</li>
+						<li class="p-b-10">
+							<a href="admin_master.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Your Admin Panel
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="rp.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Refund Policy
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="support.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Support Services
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="credits.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Credits
+							</a>
+						</li>
+				</center>
+				</div>
+
+				<div class="col-sm-6 col-lg-3 p-b-5">
+					<center><h4 class="stext-301 cl0 p-b-10" style="color:orange;">
+						INFORMATION
+					</h4>
+					</center>
+
+				<center><ul>
+					<li class="p-b-10">
+							<a href="faqs.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Frequently Asked Questions
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="tc.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Terms and Conditions
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="privacy-policy.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Privacy Policy
+							</a>
+						</li>
+
+						<li class="p-b-10">
+							<a href="disclaimer.blade.php" class="stext-107 cl7 hov-cl1 trans-04">
+								Disclaimer
+							</a>
+						</li>
+					</ul>
+				</center>
+
+			</div>
+
+				<div class="col-sm-6 col-lg-3 p-b-5">
+					<center><h4 class="stext-301 cl0 p-b-10" style="color:orange;">
+						GET IN TOUCH
+					</h4>
+					</center>
+
+					<center> <p class="stext-107 cl7 size-201">
+						Any questions? Let us know @go2domainsales.com
+					</p>
+					</center>
+
+					
+
+					<center><div class="p-t-27">
+						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fa fa-instagram"></i>
+						</a>
+
+						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fa fa-pinterest-p"></i>
+						</a>
+					</div>
+					</center>
+				</div>
+
+				</div>
+			</div>
+
+			<div class="p-t-0">
+			<center><p>We accept the following methods of payment:</p></center>
+				<div class="flex-c-m flex-w p-b-18">
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-01.png" alt="PAYPAL">
+					</a>
+
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-02.png" alt="VISA">
+					</a>
+
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-03.png" alt="MASTERCARD">
+					</a>
+
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-04.png" alt="AMEX">
+					</a>
+
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-05.png" alt="DISCOVER">
+					</a>
+
+					<a href="#" class="m-all-1">
+						<img src="images/icons/icon-pay-06.png" alt="BITCOIN">
+					</a>
+				</div>
+
+				</div>
+		</div>
+	</footer>
+
+
+	<!-- Back to top -->
+	<div class="btn-back-to-top" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<i class="zmdi zmdi-chevron-up"></i>
+		</span>
+	</div>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<script>
+		$(".js-select2").each(function(){
+			$(this).select2({
+				minimumResultsForSearch: 20,
+				dropdownParent: $(this).next('.dropDownSelect2')
+			});
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/slick/slick.min.js"></script>
+	<script src="js/slick-custom.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/parallax100/parallax100.js"></script>
+	<script>
+        $('.parallax100').parallax100();
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
+	<script>
+		$('.gallery-lb').each(function() { // the containers for all your galleries
+			$(this).magnificPopup({
+		        delegate: 'a', // the selector for gallery item
+		        type: 'image',
+		        gallery: {
+		        	enabled:true
+		        },
+		        mainClass: 'mfp-fade'
+		    });
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/isotope/isotope.pkgd.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/sweetalert/sweetalert.min.js"></script>
+	<script>
+		$('.js-addwish-b2').on('click', function(e){
+			e.preventDefault();
+		});
+
+		$('.js-addwish-b2').each(function(){
+			var nameProduct = $(this).parent().parent().find('.js-name-b2').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+
+				$(this).addClass('js-addedwish-b2');
+				$(this).off('click');
+			});
+		});
+
+		$('.js-addwish-detail').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').html();
+
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+
+				$(this).addClass('js-addedwish-detail');
+				$(this).off('click');
+			});
+		});
+
+		/*---------------------------------------------*/
+
+		$('.js-addcart-detail').each(function(){
+			var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+			});
+		});
+
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+	<script>
+		$('.js-pscroll').each(function(){
+			$(this).css('position','relative');
+			$(this).css('overflow','hidden');
+			var ps = new PerfectScrollbar(this, {
+				wheelSpeed: 1,
+				scrollingThreshold: 1000,
+				wheelPropagation: false,
+			});
+
+			$(window).on('resize', function(){
+				ps.update();
+			})
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
---
+++ public/frontend/header.php
@@ -0,0 +1,206 @@
+	<!-- Header -->
+	<header>
+		<!-- Header desktop -->
+		<div class="container-menu-desktop">
+			<!-- Topbar -->
+			<div class="top-bar">
+				<div class="content-topbar flex-sb-m h-full containerx">
+					<div class="left-top-bar">
+						<h4>We Sell Premium Domains, Hosting and Web Services.</h4>
+					</div>
+
+					<div class="right-top-bar flex-w h-full">
+						<a href="login.blade.php" class="flex-c-m trans-04 p-lr-25">
+							LOGIN
+						</a>
+
+						<a href="registration.blade.php" class="flex-c-m trans-04 p-lr-25">
+							REGISTER
+						</a>
+
+						<a href="admin_master.blade.php" class="flex-c-m trans-04 p-lr-25">
+							ACCOUNT
+						</a>
+        				<!-- <center><i class="fa" style="font-size:20px">&#xf07a;</i></center> -->
+					</div>
+				</div>
+			</div>
+<nav class="topnavbar">
+	<ul class="topnav">
+  <li><a href="index.php">HOME</a></li>
+  <li><a href="domains.blade.php">DOMAINS</a></li>
+  <li><a href="apps.blade.php">APPS</a></li>
+  <li><a href="contact.blade.php">CONTACT</a></li>
+ <li> <a href="about.blade.php">ABOUT</a></li>
+ <li> <a href="hosting.blade.php">HOSTING</a></li>
+  <li><a href="webdev.blade.php">WEB DEVELOPMENT</a></li>
+  <li><a href="support.blade.php">SUPPORT</a></li>
+  <li><a href="careers.blade.php">CAREERS</a></li>
+ <li><a href="faqs.blade.php">FAQS</a><li>
+	</ul>
+	<div class="hamburger">
+		<span class="ham"></span>
+		<span class="ham"></span>
+		<span class="ham"></span>
+		<span class="ham"></span>
+	</div>
+</nav>
+
+
+
+
+
+					<!-- Menu desktop
+					<div class="menu-desktop">
+						<ul class="main-menu">
+							<li class="active-menu">
+								<a href="index.php">Home</a>
+							</li>
+
+							<li>
+								<a href="domains.php">Domains</a>
+							</li>
+
+							<li class="label1" data-label1="hot">
+								<a href="premiumdomains.php">Featured</a>
+							</li>
+
+							<li>
+								<a href="support.php">Support</a>
+							</li>
+
+							<li>
+								<a href="about.php">About</a>
+							</li>
+
+							<li>
+								<a href="contact.php">Contact</a>
+							</li>
+						</ul>
+					</div>	 -->
+
+					<!-- Icon header -->
+					<!-- <div class="wrap-icon-header flex-w flex-r-m">
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+							<i class="zmdi zmdi-search"></i>
+						</div> -->
+
+						<!-- <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+							<i class="zmdi zmdi-shopping-cart"></i>
+						</div>
+
+						<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
+							<i class="zmdi zmdi-favorite-outline"></i>
+						</a>
+					</div>
+				</nav>
+			</div>
+		</div> -->
+
+		<!-- Header Mobile -->
+		<div class="wrap-header-mobile">
+			<!-- Logo moblie -->
+			<div class="logo-mobile">
+				<a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
+			</div>
+
+			<!-- Icon header -->
+			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
+					<i class="zmdi zmdi-search"></i>
+				</div>
+
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
+					<i class="zmdi zmdi-shopping-cart"></i>
+				</div>
+
+				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
+					<i class="zmdi zmdi-favorite-outline"></i>
+				</a>
+			</div>
+
+			<!-- Button show menu -->
+			<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</div>
+		</div>
+
+
+		<!-- Menu Mobile -->
+		<div class="menu-mobile">
+			<ul class="topbar-mobile">
+				<li>
+					<div class="left-top-bar">
+					We Sell Premium Domains, Hosting and Web Services.
+					</div>
+				</li>
+
+				<li>
+					<div class="right-top-bar flex-w h-full">
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							Help & FAQs
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							My Account
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							EN
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							USD
+						</a>
+					</div>
+				</li>
+			</ul>
+
+			<ul class="main-menu-m">
+				<li>
+					<a href="index.html">Home</a>
+				</li>
+				<li>
+					<a href="domains.php">Domains</a>
+				</li>
+
+				<li>
+					<a href="featured.php" class="label1 rs1" data-label1="new">Featured</a>
+				</li>
+
+				<li>
+					<a href="support.php">Support</a>
+				</li>
+
+				<li>
+					<a href="about.php">About</a>
+				</li>
+
+				<li>
+					<a href="contact.php">Contact</a>
+				</li>
+			</ul>
+		</div>
+
+		<!-- Modal Search -->
+		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+			<div class="container-search-header">
+				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+					<img src="images/icons/icon-close2.png" alt="CLOSE">
+				</button>
+
+				<form class="wrap-search-header flex-w p-l-15">
+					<button class="flex-c-m trans-04">
+						<i class="zmdi zmdi-search"></i>
+					</button>
+					<input class="plh3" type="text" name="search" placeholder="Search...">
+				</form>
+			</div>
+		</div>
+	</header>
+
+			</div>
+		</div>
+	</div>
